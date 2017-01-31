3a989d68ec925f57ab480996f1ecdf0ca11d2ad6
ryaoi <ryaoi@student.42.fr>
2017-02-01 00:46:11 +0100

    need this too

---
+++ ft_envtostr.c
@@ -0,0 +1,42 @@
+
+#include "minishell.h"
+
+static int	check_existenv(t_msh *msh, int *i, char **buff, char *str)
+{
+	char	*name;
+	int		j;
+
+	j = *i + 1;
+	while (str[j] >= 'A' && str[j] <= 'Z')
+		j++;
+	j -= *i + 1;
+	name = ft_strsub(str, *i + 1, j);
+	if (search_env(name, msh->env) == 1)
+	{
+		*buff = ft_strjoini(*buff, get_data(name, msh->env), 1);
+		ft_strdel(&name);
+		*i = *i + j;
+		return (1);
+	}
+	ft_strdel(&name);
+	return (0);
+}
+
+char		*envtostr(char *str, int i, t_msh **msh)
+{
+	char	*buff;
+
+	buff = ft_strnew(1);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '$')
+		{
+			if (check_existenv(*msh, &i, &buff, str) != 1)
+				buff = straddonefree(buff, str[i], 1);
+		}
+		else
+			buff = straddonefree(buff, str[i], 1);
+		i++;
+	}
+	return (buff);
+}
